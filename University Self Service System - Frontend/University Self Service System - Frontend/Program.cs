6b904aad48a3ef91a4a13c6eb4508c2f8649f19a
Youssef Essam <sofa2014098@gmail.com>
2025-11-03 18:56:51 +0200

    Add project files.

---
+++ University Self Service System - Frontend/University Self Service System - Frontend/Program.cs
@@ -0,0 +1,30 @@
+using University_Self_Service_System___Frontend.Client.Pages;
+using University_Self_Service_System___Frontend.Components;
+
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+builder.Services.AddRazorComponents()
+    .AddInteractiveWebAssemblyComponents();
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (app.Environment.IsDevelopment())
+{
+    app.UseWebAssemblyDebugging();
+}
+else
+{
+    app.UseExceptionHandler("/Error", createScopeForErrors: true);
+}
+
+
+app.UseAntiforgery();
+
+app.MapStaticAssets();
+app.MapRazorComponents<App>()
+    .AddInteractiveWebAssemblyRenderMode()
+    .AddAdditionalAssemblies(typeof(University_Self_Service_System___Frontend.Client._Imports).Assembly);
+
+app.Run();
