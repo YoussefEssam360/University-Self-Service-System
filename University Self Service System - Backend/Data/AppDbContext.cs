241644a7a694d84c1a9d1baa42c4d25af81cb8c4
Youssef Essam <sofa2014098@gmail.com>
2025-11-27 04:01:06 +0200

    Create AppDbContext.cs in Data folder Add connection string in appsettings.json Wire the DbContext in Program.cs

---
+++ University Self Service System - Backend/Data/AppDbContext.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.EntityFrameworkCore;
+using University_Self_Service_System___Backend.Entities;
+
+namespace University_Self_Service_System___Backend.Data
+{
+    public class AppDbContext : DbContext
+    {
+        public AppDbContext(DbContextOptions<AppDbContext> options)
+            : base(options)
+        {
+        }
+
+        // tables
+        public DbSet<Student> Students { get; set; }
+        public DbSet<Course> Courses { get; set; }
+        public DbSet<Professor> Professors { get; set; }
+        public DbSet<Enrollment> Enrollments { get; set; }
+        public DbSet<User> Users { get; set; }
+    }
+}
