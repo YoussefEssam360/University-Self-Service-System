2679f60db4dae4ffa5dbf0f52fb8365e45f19459
WINDKING-1 <zainosamamohamed@outlook.com>
2025-12-25 10:11:38 +0200

    added DTOs to report errors
    added DTOs to report possible phone errors,

--- University Self Service System - Backend/DTOs/ProfessorDTOs/RUpdateProfessorDto.cs
+++ University Self Service System - Backend/DTOs/ProfessorDTOs/RUpdateProfessorDto.cs
@@ -5,5 +5,6 @@ namespace University_Self_Service_System___Backend.DTOs.ProfessorDTOs
         public bool Success { get; set; } = false;
         public bool ProfessorNotFound { get; set; } = false;
         public bool EmailIsDuplicate { get; set; } = false;
+        public string? ErrorMessage { get; set; }
     }
 }
